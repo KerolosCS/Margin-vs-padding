5f29e874b722176f4033f5c8b6ce3edf42ee21bf
KerolosCS <kerolosfady4@gmail.com>
2023-10-01 21:47:42 +0300

    copy

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,3 +1,15 @@
+/*
+ *
+ * ----------------
+ * | 241030072002 |
+ * ----------------
+ * Copyright © [2023] KERO CS FLUTTER DEVELOPMENT.
+ * All Rights Reserved. For inquiries or permissions, contact  me ,
+ * https://www.linkedin.com/in/kerolos-fady-software-engineer/
+ *
+ * /
+ */
+
 // This is a basic Flutter widget test.
 //
 // To perform an interaction with a widget in your test, use the WidgetTester
